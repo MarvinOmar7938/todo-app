8e01ce3f3038d49282a3e9ae9b4cd6af2987ea12
marvin omar tola pacheco <mtolapacheco@gmail.com>
2019-09-02 23:25:42 -0400

    create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule
